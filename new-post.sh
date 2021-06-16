c8da28a9c9fdb55259904f60d5a6d7e54f471bc1
Duncan Lock <duncan.lock@gmail.com>
2021-06-16 13:55:47 -0700

    Add new post script & fix dates

--- new-post.sh
+++ new-post.sh
@@ -14,6 +14,7 @@ function init() {
   readonly script_name="$(basename "$script_path")"
 
   tags=""
+  category=""
 
   setup_colors
   parse_params "$@"
@@ -22,7 +23,7 @@ function init() {
 function usage() {
   cat <<EOF
 
-Create a new TIL post for the blog
+Create a new post for the blog
 
 ${bld}USAGE${off}
   $script_name [options] TITLE
@@ -30,13 +31,14 @@ ${bld}USAGE${off}
 ${bld}OPTIONS${off}
   -h, --help       show this help
   -t, --tags       tags for this post. Comma separated, no spaces.
+  -c, --category   category for the post, no spaces. If none, just saved into /posts folder.
 
 ${bld}ARGUMENTS${off}
-  TITLE     the title of the TIL post. Spaces are allowed.
+  TITLE     the title of the post. Spaces are allowed.
 
 ${bld}EXAMPLES${off}
-  ${gry}# Create a new post called "Promise.allSettled in JavaScript"${off}
-  $ $script_name --tags javascript,es6 Promise.allSettled in JavaScript
+  ${gry}# Create a new post called "Basic Pickles or Fermented Cucumbers Recipe"${off}
+  $ $script_name --tags fermentation,food --category 'home & garden' Basic Pickles or Fermented Cucumbers Recipe
 EOF
   exit
 }
@@ -92,6 +94,10 @@ function parse_params() {
         tags="$2"
         shift
         ;;
+      -c | --category)
+        category="$2"
+        shift
+        ;;
       *)
         title="${title:-} $param"
         ;;
@@ -100,9 +106,10 @@ function parse_params() {
   done
   title=$(trim "${title:-}")
   title_slug=$(echo "$title" | slugify)
-  post="$script_dir/content/posts/tech/til/$title_slug.adoc"
-  if [[ $tags ]]; then
-    tags=",$tags"
+  if [[ $category ]]; then
+    post="$script_dir/content/posts/$category/$title_slug.adoc"
+  else
+    post="$script_dir/content/posts/$title_slug.adoc"
   fi
 }
 
@@ -118,11 +125,11 @@ cat << EOF > "$post"
 
 :slug: $title_slug
 :date: $(date --rfc-3339=s)
-:tags: til$tags
-:category: tech
+:tags: $tags
 :meta_description: 
 
 
-== References
+---
+=== Footnotes & References
 
 EOF
