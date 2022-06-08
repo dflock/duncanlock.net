352957c5639bb38957822c6704931538f391d9f6
Duncan Lock <duncan.lock@gmail.com>
2022-06-08 09:23:15 -0700

    Run black on publishconf

--- publishconf.py
+++ publishconf.py
@@ -12,13 +12,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # Use real live URLs
-SITESCHEME = 'https'
-SITEURL = SITESCHEME + '://duncanlock.net'
+SITESCHEME = "https"
+SITEURL = SITESCHEME + "://duncanlock.net"
 RELATIVE_URLS = False
 
 # Send the published output to a different folder, so It doesn't
 # overwrite the dev site. It's just used for staging before uploading, really.
-OUTPUT_PATH = 'published_output/'
+OUTPUT_PATH = "published_output/"
 # Leave the cache alone when publishing
 CACHE_CONTENT = False
 LOAD_CONTENT_CACHE = False
@@ -31,11 +31,11 @@ RSS_FEED_SUMMARY_ONLY = False
 # it is highly recommended to define this.
 FEED_DOMAIN = SITEURL
 
-FEED_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
-FEED_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
+FEED_RSS = "feeds/all.rss.xml"
+CATEGORY_FEED_RSS = "feeds/{slug}.rss.xml"
 
 # Always start over
 DELETE_OUTPUT_DIRECTORY = True
@@ -59,19 +59,11 @@ GOOGLE_ANALYTICS_UNIVERSAL = True
 #################################
 
 # Which extra plugins to enable when publishing
-PLUGINS = PLUGINS + ['gzip_cache', 'sitemap']
+PLUGINS = PLUGINS + ["gzip_cache", "sitemap"]
 
 # Settings for the sitemap plugin
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.8,
-        'indexes': 0.7,
-        'pages': 0.5
-    },
-    'changefreqs': {
-        'articles': 'daily',
-        'indexes': 'weekly',
-        'pages': 'monthly'
-    }
+    "format": "xml",
+    "priorities": {"articles": 0.8, "indexes": 0.7, "pages": 0.5},
+    "changefreqs": {"articles": "daily", "indexes": "weekly", "pages": "monthly"},
 }
